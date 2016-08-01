a75ca7b25bda3b6fdc8e8fad14d2e39673c794cc
李宝昌 <18722676963@163.com>
2016-08-01 14:47:35 +0800

    add test 01 02 03 04 05

---
+++ Test02/ex2.rb
@@ -0,0 +1,3 @@
+#puts 'A comment, this is so you can read your program later'
+puts 'This will be read'
+
---
+++ Test05/ex5.rb
@@ -0,0 +1,8 @@
+my_name = "ibcLee"
+my_age = "23"
+my_eyes = "Black"
+my_hair = "Brown"
+
+puts "Let's talk about %s." % my_name
+puts "He's %d years old." % my_age
+puts "He's got %s eyes and %s hair." % [my_eyes, my_hair]
