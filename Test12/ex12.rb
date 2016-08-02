56be0a9473167ece11c9c3778a267f05f0e4803e
李宝昌 <18722676963@163.com>
2016-08-02 16:08:57 +0800

    add example 12-20

---
+++ Test12/ex12.rb
@@ -0,0 +1,12 @@
+#從現在開始我們將把這些我們 require 進來的功能稱作 modules（模組）
+
+require 'open-uri'
+
+open("http://www.ruby-lang.org/en") do |f|
+  f.each_line {|line| p line}
+  puts f.base_uri         # <URI::HTTP:0x40e6ef2 URL:http://www.ruby-lang.org/en/>
+  puts f.content_type     # "text/html"
+  puts f.charset          # "iso-8859-1"
+  puts f.content_encoding # []
+  puts f.last_modified    # Thu Dec 05 02:45:02 UTC 2002
+end
---
+++ Test13/ex13.rb
@@ -0,0 +1,6 @@
+first, second, third = ARGV
+# ARGV就是变数参数
+puts "The script is called: #{$0}"
+puts "Your first variable is: #{first}"
+puts "Your second variable is: #{second}"
+puts "Your third variable is: #{third}"
